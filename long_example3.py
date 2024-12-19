23474be1f80089ce15edfe155a506ec3fae5ed33
Маргарита Горбутова <weadid@Noutbuk-Margarita.local>
2024-12-19 17:22:15 +0300

    2provercs

--- long_example3.py
+++ long_example3.py
@@ -1,5 +1,5 @@
 def fibonacci(n):
-    if n <= 0:  # E501 line too long (27 > 79 characters)
+    if n <= 0:  # Проверка на неотрицательное значение
         return []
     elif n == 1:
         return [0]
@@ -9,12 +9,13 @@ def fibonacci(n):
         fib_sequence = [0, 1]
         for i in range(2, n):
             next_fib = fib_sequence[i - 1] + fib_sequence[i - 2]
-            fib_sequence.append(next_fib)  # E501 line too long (44 > 79 characters)
+            fib_sequence.append(next_fib)
         return fib_sequence
+
 
 # Пример использования функции
 n = 10
-print("Fibonacci sequence up to", n, ":", fibonacci(n))  # E501 line too long (54 > 79 characters)
+fibonacci_sequence = fibonacci(n)
+print(f"Fibonacci sequence up to {n}: {fibonacci_sequence}")
 
-# Комментарий без пробела
-#Это пример функции
+# Это пример функции
